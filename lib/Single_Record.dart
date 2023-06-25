9a8b9f41e1c1562b5aa0c0a20439c11db6b24b79
Chamath Harshana <it21175466@my.sliit.lk>
2023-06-25 17:09:03 +0530

    Update Single_Record.dart

--- lib/Single_Record.dart
+++ lib/Single_Record.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
