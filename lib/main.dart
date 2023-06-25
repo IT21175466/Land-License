2e7a551684362074473d7adbe5c7f8cf48154d7c
Chamath Harshana <it21175466@my.sliit.lk>
2023-06-25 16:20:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:land_license/Add_License_Page.dart';
 import 'package:land_license/Home_Page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:land_license/Search_License.dart';
-import 'package:land_license/Single_Record.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
